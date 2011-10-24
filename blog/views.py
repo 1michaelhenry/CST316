98e04a0fe1ed1296c9499310272eb931691314ce
Michael Henry <1michaelhenry@gmail.com>
2011-10-24 16:17:52 +0000

    Added some other stuff

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,19 @@
 # Create your views here.
 from django.shortcuts import render_to_response
+from django.template import RequestContext
+from django.http import HttpResponse
 
 from CST316.blog.models import posts
 
 def home(request):
         entries = posts.objects.all()[:10]
-        return render_to_response('index.html', {'posts' : entries })
+        return render_to_response('index.html', {'posts' : entries }, \
+			context_instance=RequestContext(request))
 
+def login(request):
+	return render_to_response('login.html', \
+			context_instance=RequestContext(request))
+
+def auth(request):
+	entries = posts.objects.all()[:10]
+	return render_to_response('auth.html', {'posts' : entries })
--- urls.py
+++ urls.py
@@ -9,6 +9,8 @@ urlpatterns = patterns('',
     # url(r'^$', 'FirstBlog.views.home', name='home'),
     # url(r'^FirstBlog/', include('FirstBlog.foo.urls')),
     url(r'^$', 'CST316.blog.views.home', name='home'),
+    url(r'^login', 'CST316.blog.views.login', name='login'),
+    url(r'^auth', 'CST316.blog.views.auth', name='auth'),
     # Uncomment the admin/doc line below to enable admin documentation:
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
