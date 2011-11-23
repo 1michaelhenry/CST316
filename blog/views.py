ddbf0f58c22d69224571967f4db3f4a222d76feb
Michael Henry <1michaelhenry@gmail.com>
2011-11-22 21:21:20 -0500

    Added template files

--- blog/views.py
+++ blog/views.py
@@ -6,7 +6,7 @@ from django.http import HttpResponse
 from CST316.blog.models import posts
 
 def home(request):
-        entries = posts.objects.all()[:10]
+        entries = posts.objects.all()[:10:-1]
         return render_to_response('index.html', {'posts' : entries }, \
 			context_instance=RequestContext(request))
 
@@ -15,7 +15,7 @@ def login(request):
 			context_instance=RequestContext(request))
 
 def auth(request):
-	entries = posts.objects.all()[:10]
+	entries = posts.objects.all()[:10:-1]
 	return render_to_response('auth.html', {'posts' : entries })
 
 def test(request):
