42ca8688df4fcdcf7c200d255d5ae24707a54b94
Michael Henry <1michaelhenry@gmail.com>
2011-11-23 14:55:54 +0000

    Added some more functionality for users viewing their pages

--- blog/views.py
+++ blog/views.py
@@ -4,11 +4,12 @@ from django.template import RequestContext
 from django.http import HttpResponse
 from django.http import HttpResponseRedirect
 from django.contrib import auth
-
+import datetime
 from CST316.blog.models import posts
 
-def getblog(request):
-	entries = posts.objects.filter(author=request.POST['author'])
+def getauthblog(request):
+	entries = posts.objects.filter(author=request.session['username']).order_by('-timestamp')
+	entries = entries[:10]
 	return entries
 
 def home(request):
@@ -20,26 +21,36 @@ def logout(request):
 	return HttpResponseRedirect('/')
 
 def loginerror(request):
-	return render_to_response('loginerror.html', context_instance=RequestContext(request))
+	return render_to_response(
+		'loginerror.html',
+		context_instance=RequestContext(request))
 
 def authorize(request):
-	user = auth.authenticate(username=request.POST['username'], password=request.POST['password'])
+	user = auth.authenticate(
+		username=request.POST['username'],
+		password=request.POST['password'])
 	if user is not None:
 		request.session['username'] = request.POST['username']
-		entries = posts.objects.filter(author=request.session['username'])
-		return render_to_response('auth.html', {'posts' : entries, 'user':request.session['username']}, \
-		context_instance=RequestContext(request))
+		entries = getauthblog(request)
+		return render_to_response(
+			'auth.html',
+			{'posts':entries,
+			 'user' :request.session['username']},
+			context_instance=RequestContext(request))
 	else:
 		return HttpResponseRedirect('/loginerror')
 
-def test(request):
-	return render_to_response('test.html', \
-			context_instance=RequestContext(request))
-
 def postblog(request):
-	post = posts(author=request.session['username'], title=request.POST['title'], bodytext=request.POST['bodytext'])
+	post = posts(
+		author=request.session['username'],
+		title=request.POST['title'],
+		bodytext=request.POST['bodytext'],
+		timestamp=datetime.datetime.now())
 	post.save()
-	entries = posts.objects.filter(author=request.session['username'])[:10:-1]
-	return render_to_response('auth.html', {'posts' : entries, 'user':request.session['username']}, \
+	entries = getauthblog(request)
+	return render_to_response(
+		'auth.html',
+		{'posts' : entries,
+		'user':request.session['username']},
 		context_instance=RequestContext(request))
 
